62aa8efd42f28a1b9dc440a4f45099793348231b
Marco Janssen <marco.janssen@outlook.com>
2013-06-16 10:27:17 +0200

    Init only includes auges & apt update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,5 @@
 class project {
 
     include augeas
-
     include project::apt
-    include project::composer
-    include project::web
-    include project::php5
-    include project::pear
-    include project::phing
-    include project::sql
-
-    if ($project-lamp) {
-        include project::varnish
-    }
-
-    if ($project-jenkins) {
-        include project::jenkins
-    }
 }
