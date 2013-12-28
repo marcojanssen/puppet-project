0125df735d8578f7d86472671d5cd78af789bc6d
Marco Janssen <dev@marcojanssen.net>
2013-12-28 22:33:23 +0100

    Added adminer

---
+++ manifests/adminer.pp
@@ -0,0 +1,22 @@
+class project::adminer {
+
+    exec { "mkdir-adminer":
+        command => "mkdir -p /var/www/adminer/web",
+        path    => ['/bin'],
+        unless  => "/usr/bin/test -d /var/www/adminer/web"
+    }
+
+    exec { "download-adminer":
+        command => "wget http://downloads.sourceforge.net/adminer/adminer-3.7.1.php",
+        cwd     => '/var/www/adminer/web',
+        path    => ['/usr/bin', '/usr/sbin'],
+        require => Exec['mkdir-adminer']
+    }
+
+    exec { "rename-adminer":
+        command => "mv adminer-3.7.1.php index.php",
+        cwd     => '/var/www/adminer/web',
+        path    => ['/bin'],
+        require => Exec['download-adminer']
+    }
+}
