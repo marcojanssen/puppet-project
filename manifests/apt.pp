223f403e25e1fa24dcb18fc69b90b0377bb39b86
Marco Janssen <dev@marcojanssen.net>
2013-12-28 16:52:02 +0100

    Added dotdeb repository

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -5,4 +5,9 @@ class project::apt {
 
     # Ensure apt-get update has been run before installing any packages
     Exec["apt-update"] -> Package <| |>
+
+    exec { "adding-dotdeb":
+        command => "echo 'deb http://packages.dotdeb.org wheezy all' >> /etc/apt/sources.list && echo 'deb-src http://packages.dotdeb.org wheezy all' >> /etc/apt/sources.list && wget http://www.dotdeb.org/dotdeb.gpg && sudo apt-key add dotdeb.gpg",
+        before  => Exec['apt-update']
+    }
 }
