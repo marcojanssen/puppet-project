5873cd3ef631b5249f7475457505b5c834074f13
Marco Janssen <dev@marcojanssen.net>
2013-12-28 22:00:19 +0100

    Added default .bashrc & .vimrc

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,24 @@ class project {
 
     include augeas
     include project::apt
+
+    file {
+      "/home/vagrant/.bashrc":
+      ensure  => present,
+      require => Package["varnish"],
+      source  => "puppet:///modules/project/.bashrc",
+      notify  => Service["varnish"],
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644';
+
+      "/home/vagrant/.vimrc":
+      ensure => present,
+      require => Package["varnish"],
+      source => "puppet:///modules/project/.vimrc",
+      notify  => Service["varnish"],
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644';
+    }
 }
