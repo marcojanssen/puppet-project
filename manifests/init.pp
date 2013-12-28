638bae4030317b8c8bbc4df42f760b87f5f11df9
Marco Janssen <dev@marcojanssen.net>
2013-12-28 22:48:09 +0100

    Removed vimrc, still not working properly

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,13 +9,6 @@ class project {
       source  => "puppet:///modules/project/.bashrc",
       owner   => 'vagrant',
       group   => 'vagrant',
-      mode    => '0644';
-
-      "/home/vagrant/.vimrc":
-      ensure => present,
-      source => "puppet:///modules/project/.vimrc",
-      owner   => 'vagrant',
-      group   => 'vagrant',
-      mode    => '0644';
+      mode    => '0644'
     }
 }
