2c92d36e3f1e3839ef7838ede5502d89b94ed63a
Roberto Gardenier <r.gardenier@simgroep.nl>
2014-05-05 12:42:28 +0200

    Fixed indentation

--- manifests/pdflib.pp
+++ manifests/pdflib.pp
@@ -4,35 +4,35 @@ class project::pdflib {
 		require => [ Package["php"] ],
 		ensure => "directory",
 		owner   => 'root',
-        group   => 'root'
+		group   => 'root'
 	}
 
-    file { "/etc/php5/extensions/php_pdflib.so":
+	file { "/etc/php5/extensions/php_pdflib.so":
 		require => [ Package["php"] ],
-        ensure  => present,
-        source  => "puppet:///modules/project/pdflib/php_pdflib.so", 
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644'
-    }
+		ensure  => present,
+		source  => "puppet:///modules/project/pdflib/php_pdflib.so", 
+		owner   => 'root',
+		group   => 'root',
+		mode    => '0644'
+	}
 
 	file { "/etc/php5/mods-available/pdflib.ini":
 		require => [ Package["php"] ],
-        ensure  => present,
-        source  => "puppet:///modules/project/pdflib/pdflib.ini", 
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644'
-    }
-	
+		ensure  => present,
+		source  => "puppet:///modules/project/pdflib/pdflib.ini", 
+		owner   => 'root',
+		group   => 'root',
+		mode    => '0644'
+	}
+
 	file { '/etc/php5/conf.d/pdflib.ini':
 		require => [ Package["php"] ],
 		ensure => 'link',
 		target => '/etc/php5/mods-available/pdflib.ini'
 	}
-	
-    exec { "restart-apache":
+
+	exec { "restart-apache":
 		require => [ Package["php"] ],
-        command => "sudo service apache2 restart"
-    }
+		command => "sudo service apache2 restart"
+	}
 }
