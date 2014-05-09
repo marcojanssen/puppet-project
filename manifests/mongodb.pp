dcfc19a4b319e478f5e681d27ba305f2f4a5f1a5
Marco Janssen <dev@marcojanssen.net>
2014-05-09 13:29:44 +0200

    Updated mongo installation

--- manifests/mongodb.pp
+++ manifests/mongodb.pp
@@ -1,38 +1,29 @@
 class project::mongodb {
 
     exec { "mongodb-apt-get-key":
-        command => "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
+        command => "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10",
         before  => Exec['apt-update']
     }
 
     exec { "mongodb-apt-get-sources":
-        command => "echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list",
+        command => "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list",
         creates => '/etc/apt/sources.list.d/mongodb.list',
         require => Exec["mongodb-apt-get-key"],
         before  => Exec['apt-update']
     }
 
-    package { "mongodb-10gen":
+    package { "mongodb-org":
         ensure   => present,
         require  => Exec["apt-update"]
     }
 
-    package { "php5-dev":
+    package { "php5-mongo":
         ensure   => present,
         require  => [
             Exec["apt-update"],
-            Package["php"]
+            Package["php"],
+            Package["mongodb-org"]
         ]
-    }
-
-    exec { "php-mongo-driver":
-        command => "pecl install mongo",
-        require => [
-            Package['mongodb-10gen'],
-            Package['php5-dev'],
-            Exec['pear-auto-discover']
-        ],
-        user => root
     }
 
     file {
