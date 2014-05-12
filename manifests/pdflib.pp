98dc92dea9fe20f5e2f3c05804245de9adb78e8e
Marco Janssen <dev@marcojanssen.net>
2014-05-12 10:24:06 +0200

    Added symlinks & php5.5 version of pdflib

--- manifests/pdflib.pp
+++ manifests/pdflib.pp
@@ -20,10 +20,22 @@ class project::pdflib {
     file { "/etc/php5/mods-available/pdflib.ini":
         require => [ Package["php5"] ],
         ensure  => present,
-        source  => "puppet:///modules/project/pdflib/pdflib.ini", 
+        source  => "puppet:///modules/project/pdflib/pdflib.ini",
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
+        notify  => Service["apache"]
+    }
+
+    exec { "symlink-pdflib-apache2":
+        command => "ln -s /etc/php5/mods-available/pdflib.ini /etc/php5/apache2/conf.d/pdflib.ini",
+        require => File['/etc/php5/mods-available/pdflib.ini'],
+        notify  => Service["apache"]
+    }
+
+    exec { "symlink-pdflib-cli":
+        command => "ln -s /etc/php5/mods-available/pdflib.ini /etc/php5/cli/conf.d/pdflib.ini",
+        require => File['/etc/php5/mods-available/pdflib.ini'],
         notify  => Service["apache"]
     }
 }
