a28f5265ff6f59fb34e9fd01b2b4ae940983551a
Marco Janssen <dev@marcojanssen.net>
2014-05-13 15:27:44 +0200

    Fixed mcrypt symlink, wasn't enabled by default

--- manifests/php5.pp
+++ manifests/php5.pp
@@ -7,10 +7,25 @@ class project::php5 {
     package { 'php5-intl': ensure   => present, require  => Package["php5-common"] }
     package { 'php5-imagick': ensure   => present, require  => Package["php5-common"] }
     package { 'php5-gd': ensure   => present, require  => Package["php5-common"] }
-    package { 'php5-mcrypt': ensure   => present, require  => Package["php5-common"] }
+    package { 'mcrypt': ensure   => present, require  => Package["php5-common"] }
+    package { 'php5-mcrypt': ensure   => present, require  => Package["mcrypt"] }
     package { 'php5-mysql': ensure   => present, require  => Package["php5-common"] }
     package { 'php5-sqlite': ensure   => present, require  => Package["php5-common"] }
     package { 'php5-xcache': ensure   => present, require  => Package["php5-common"] }
+
+    exec { "symlink-mcrypt-apache2":
+        command => "ln -sf /etc/php5/mods-available/mcrypt.ini /etc/php5/apache2/conf.d/mcrypt.ini",
+        require => Package['php5-mcrypt'],
+        creates => "/etc/php5/apache2/conf.d/mcrypt.ini",
+        notify  => Service["apache"]
+    }
+
+    exec { "symlink-mcrypt-cli":
+        command => "ln -sf /etc/php5/mods-available/mcrypt.ini /etc/php5/cli/conf.d/mcrypt.ini",
+        require => Package['php5-mcrypt'],
+        creates => "/etc/php5/cli/conf.d/mcrypt.ini",
+        notify  => Service["apache"]
+    }
 
     file {
         "/etc/php5/apache2/php.ini":
